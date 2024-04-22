e7ff6328ae57c5a762ef6b066af2471851b16c2a
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-04-22 11:38:26 -0500

    Graded Assignment #1: Tech Jobs (Console Edition)

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,17 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+       // System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.print("No Results");
+            return;
+        }
+        for (HashMap<String,String> job:someJobs){
+            System.out.println("\n*****");
+            for (String keyVal: job.keySet()){
+                System.out.println(keyVal+": "+job.get(keyVal));
+            }
+            System.out.println("*****");
+        }
     }
 }
